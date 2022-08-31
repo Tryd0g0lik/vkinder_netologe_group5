7def37eb058c207a999d60099e5be951a1e1bd79
Tryd0g0lik <work80@mail.ru>
2022-08-31 11:02:46 +0700

    class from sql majing

--- config.py
+++
@@ -1,9 +0,0 @@
-import os
-from dotenv import load_dotenv
-
-load_dotenv()
-
-DSN = os.getenv("DSN")
-TOKEN_BOT = os.getenv("TOKEN_BOT")
-TOKEN_API_VK = os.getenv("TOKEN_API_VK")
-VERSION_API_VK = os.getenv("VERSION_API_VK")
